e8edb0813e47dd1db08834ea379417e871cf8ef8
[SANGLMIN] <[snt.oceaner@gmail.com]>
2019-11-06 01:48:25 +0900

    Design Scheduling Process

---
+++ Scheduling_Scheduler.py
@@ -0,0 +1,32 @@
+#sudo
+import queue
+from Scheduling_Setup import *
+from Scheduling_Event import *
+from Scheduling_Archi import *
+from threading import *
+
+
+#TODO
+# Input Output 설정 (Pasing X Packet O)
+# order -> cook
+
+
+#initialize
+#
+def main() :
+    #load information
+    #pasing
+
+    foodInfo = loadFood()
+
+
+
+    #Scheduling
+
+    #output
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
